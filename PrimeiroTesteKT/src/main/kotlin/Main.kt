56e9170c39aa8c3d045e11a86d8b9a853b620fda
DuduFN <dudu.fn1364@gmail.com>
2022-02-24 17:34:44 -0300

    PrimeiroKT

---
+++ PrimeiroTesteKT/src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+fun main() {
+
+    println("Geraldão - Oba!")
+    println("Cornélios - Opa!")
+    println("Geraldão - Bão?")
+    println("Cornélios - Bão, e ocê, sô?")
+    println("Geraldão - Bão também.. 😦 ")
+    println("Cornélios - E cadê a cumadi?")
+    println("Geraldão - Homi, cê num sabe..")
+    println("Cornélios - Sei mesmo não..")
+    println("Geraldão - Peguei ela fugindo da casa de Ricardo, pela janela.. sem um sapato")
+    println("Cornélios - Mentira?? :)")
+    println("Geraldão - Pois é, homem, eu tô arrasado")
+    println("Cornélios - Eu avisei que Ricardo não prestava..")
+    println("Geraldão - Eu nunca imaginei, sabe..")
+    println("Cornélios - Mas fique tranquilo, seu Geraldo, um homem sem chifre é um homem indefeso..")
+    println("Geraldo se levanta e sai andando em silêncio")
+}
