7a2a182fa01d56174814dc39c3f1fa1503bee959
DuduFN <dudu.fn1364@gmail.com>
2022-02-24 17:12:55 -0300

    AjustesCódigos

--- Loop/src/main/kotlin/Exc_02.kt
+++ Loop/src/main/kotlin/Exc_02.kt
@@ -1,3 +1,17 @@
 fun main(){
+    var numP = 0
+    var numI = 0
 
+    for (i in 1..10){
+        print("Digite o ${i}° número: ")
+        val num = readLine()!!.toInt()
+
+        if (num % 2 == 0){
+            numP++
+        }else{
+            numI++
+        }
+    }
+    println("A quantidade de números impares é: $numI")
+    println("A quantidade de números pares é: $numP")
 }
